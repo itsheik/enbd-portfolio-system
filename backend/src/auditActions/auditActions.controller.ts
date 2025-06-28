df0116da6f80f4532b32c11841129a6887861fa6
Ahmed Okasha <aokasha1@outlook.com>
2025-06-28 15:35:14 +0400

    adding the aduit actions api

--- backend/src/auditActions/auditActions.controller.ts
+++ backend/src/auditActions/auditActions.controller.ts
@@ -17,11 +17,7 @@ export class AuditActionsController implements Controller {
         try {
             const page = req.query.page ? parseInt(req.query.page as string, 10) : 1;
             const pageSize = req.query.pageSize ? parseInt(req.query.pageSize as string, 10) : 10;
-            const userLoginDetailId = req.query.userLoginDetailId ? parseInt(req.query.userLoginDetailId as string, 10) : undefined;
-            const userAction = req.query.userAction ? (req.query.userAction as string) : undefined;
-            const START_DATE_TIME = req.query.startDate ? new Date(req.query.startDate as string) : undefined;
-            const END_DATE_TIME = req.query.endDate ? new Date(req.query.endDate as string) : undefined;
-            const result = await this.getAuditActionsUseCase.execute({ page, pageSize, userLoginDetailId, userAction, START_DATE_TIME, END_DATE_TIME });
+            const result = await this.getAuditActionsUseCase.execute({ page, pageSize});
 
             res.json(result);
         } catch (error) {
